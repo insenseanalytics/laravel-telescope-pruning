de1d0eaa8e7c8227e45191734e8467b5a77e3b81
Paras Malhotra <paras@insenseanalytics.com>
2018-10-31 22:32:06 +0530

    added config publishing

--- src/TelescopePruningServiceProvider.php
+++ src/TelescopePruningServiceProvider.php
@@ -12,6 +12,7 @@ class TelescopePruningServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
+            $this->registerPublishing();
             return;
         }
         
@@ -29,5 +30,17 @@ class TelescopePruningServiceProvider extends ServiceProvider
             __DIR__.'/../config/telescope-pruning.php',
             'telescope-pruning'
         );
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function registerPublishing()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/telescope-pruning.php' => config_path('telescope-pruning.php'),
+        ], 'telescope-pruning-config');
     }
 }
